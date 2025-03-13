a3a6c8d7c1207a52fd6a54b8a4bb096a926b6420
rishi <128016062+sharmarishikesh1215@users.noreply.github.com>
2025-03-13 20:05:29 +0000

    data types added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+/*
+Primitive Datatype
+--------------------
+number => 2 to the power 55
+bigInt
+string
+null => stand alone used for var like temp as we can't assign 0 if there isn't any value
+undefined
+
+Non-primitive
+---------------
+object
+
+*/
+
+console.log(typeof "rishi")
+const num = 522
+console.log(typeof num);
+console.log(typeof null); // object
+console.log(typeof undefined);
+
