7c7cbe0ecbb979ed90bdd9b3a5bf1396a87496ec
rishi <128016062+sharmarishikesh1215@users.noreply.github.com>
2025-03-16 05:11:24 +0000

    comparisons added

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+// Always compare variables with same data types
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
