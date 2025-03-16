90bd184fe431da4fda8a51272d30738f4edc3472
rishi <128016062+sharmarishikesh1215@users.noreply.github.com>
2025-03-16 04:59:05 +0000

    operations added

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,31 @@
+// Conversions
+
 let string = "12312282"
 let num = Number(string)
-console.log(typeof num)
+// console.log(typeof num)
 
 let string1 = "12312282abc"
 let num1 = Number(string1)
-console.log(typeof num1)
-console.log(num1)
+// console.log(typeof num1)
+// console.log(num1)
 
 let string2 = null
 let num2 = Number(string2)
-console.log(typeof num2)
-console.log(num2)
+// console.log(typeof num2)
+// console.log(num2)
 
 let string3 = undefined
 let num3 = Number(string3)
-console.log(typeof num3)
-console.log(num3)
+// console.log(typeof num3)
+// console.log(num3)
 
 let isLoggedIn = null
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
+// Operations
 
+console.log("2" + 1 + 2)
+console.log(2 + 1 + "2")
+console.log(+true) // 1
+console.log(+"") // 0
